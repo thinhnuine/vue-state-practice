a41dd7e51114f825a905d93d8ad2c6917390dfe0
Thinh Nguyen <thinhnguyen@Ducs-MacBook-Pro.local>
2022-08-01 22:20:09 +0700

    antd

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,13 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import Antd from 'ant-design-vue'
 import './index.css'
+import 'ant-design-vue/dist/antd.css'
 
-createApp(App).mount('#app')
+// createApp(App).mount('#app')
+
+const app = createApp(App)
+
+app.use(Antd)
+app.mount('#app')
